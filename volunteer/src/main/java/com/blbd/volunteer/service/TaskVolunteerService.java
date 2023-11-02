42587740bc921c41f1261d91328aa3cb24d95476
ZincIon <1185911254xj@gmail.com>
2023-11-02 21:24:35 +0800

    新增TaskVolunteerService内容

--- volunteer/src/main/java/com/blbd/volunteer/service/TaskVolunteerService.java
+++ volunteer/src/main/java/com/blbd/volunteer/service/TaskVolunteerService.java
@@ -1,4 +1,14 @@
 package com.blbd.volunteer.service;
 
+import com.blbd.volunteer.dao.entity.TaskVolunteerEntity;
+
+import java.util.List;
+
 public interface TaskVolunteerService {
+    //志愿者接受任务
+    int volunteerAcceptTask(TaskVolunteerEntity taskVolunteerEntity);
+    //修改任务-志愿者
+    int modifyTaskVolunteer(TaskVolunteerEntity taskVolunteerEntity);
+    //查询任务-志愿者
+    List<TaskVolunteerEntity> selectTaskVolunteer(TaskVolunteerEntity taskVolunteerEntity);
 }
--- volunteer/src/main/java/com/blbd/volunteer/service/impl/TaskVolunteerImpl.java
+++ volunteer/src/main/java/com/blbd/volunteer/service/impl/TaskVolunteerImpl.java
@@ -1,8 +1,40 @@
 package com.blbd.volunteer.service.impl;
 
+import com.blbd.volunteer.dao.TaskVolunteerEntityMapper;
+import com.blbd.volunteer.dao.entity.TaskVolunteerEntity;
 import com.blbd.volunteer.service.TaskVolunteerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+
+import java.util.List;
 
 @Service
 public class TaskVolunteerImpl implements TaskVolunteerService {
+    @Autowired
+    TaskVolunteerEntityMapper taskVolunteerEntityMapper;
+
+    //新增任务-志愿者
+    public int volunteerAcceptTask(TaskVolunteerEntity taskVolunteerEntity) {
+        if(taskVolunteerEntityMapper.insert(taskVolunteerEntity) == 1) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    //修改任务志愿者
+    public int modifyTaskVolunteer(TaskVolunteerEntity taskVolunteerEntity) {
+        if(taskVolunteerEntityMapper.update(taskVolunteerEntity) == 1) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    //查询任务-志愿者
+    public List<TaskVolunteerEntity> selectTaskVolunteer(TaskVolunteerEntity taskVolunteerEntity) {
+        return taskVolunteerEntityMapper.selectAll(taskVolunteerEntity);
+    }
+
 }

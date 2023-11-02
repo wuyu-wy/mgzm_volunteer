53f35107d92cbe90336aecf0fa5a7847c69fb9bb
ZincIon <1185911254xj@gmail.com>
2023-11-02 20:39:15 +0800

    新增任务-志愿者mapper及其测试

--- volunteer/src/main/java/com/blbd/volunteer/dao/TaskVolunteerEntityMapper.java
+++ volunteer/src/main/java/com/blbd/volunteer/dao/TaskVolunteerEntityMapper.java
@@ -1,8 +1,8 @@
 package com.blbd.volunteer.dao;
 
 
+import com.blbd.volunteer.dao.entity.TaskVolunteerEntity;
 import org.apache.ibatis.annotations.Mapper;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,5 +11,13 @@ import java.util.List;
 @Mapper
 @Repository
 public interface TaskVolunteerEntityMapper {
+    //接受任务
+    int insert(TaskVolunteerEntity taskVolunteerEntity);
+
+    //修改任务信息
+    int update(TaskVolunteerEntity taskVolunteerEntity);
+
+    //查询任务信息
+    List<TaskVolunteerEntity> selectAll(TaskVolunteerEntity taskVolunteerEntity);
 
 }
--- volunteer/src/test/java/com/blbd/volunteer/TaskVolunteerApplicationTests.java
+++ volunteer/src/test/java/com/blbd/volunteer/TaskVolunteerApplicationTests.java
@@ -1,16 +1,61 @@
 package com.blbd.volunteer;
 
+import com.blbd.volunteer.dao.TaskVolunteerEntityMapper;
 import com.blbd.volunteer.dao.VolunteerEntityMapper;
+import com.blbd.volunteer.dao.entity.OrganizationEntity;
+import com.blbd.volunteer.dao.entity.TaskVolunteerEntity;
 import com.blbd.volunteer.dao.entity.VolunteerEntity;
+import lombok.Data;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 @SpringBootTest(classes = VolunteerApplication.class)
 @RunWith(SpringRunner.class)
 class TaskVolunteerApplicationTests {
+    @Autowired
+    TaskVolunteerEntityMapper taskVolunteerEntityMapper;
+
+    @Test
+    public void testInsert() {
+        TaskVolunteerEntity taskVolunteerEntity = new TaskVolunteerEntity();
+        taskVolunteerEntity.setVolunteerId("1");
+        taskVolunteerEntity.setTaskId("1");
+        taskVolunteerEntity.setChildId("1");
+        taskVolunteerEntity.setApprovalStartTime(new Date(System.currentTimeMillis()));
+        taskVolunteerEntity.setIsCompletedApproval((byte) 0);
+        int res = taskVolunteerEntityMapper.insert(taskVolunteerEntity);
+        assert res == 1;
+    }
+
+    @Test
+    public void testUpdate() {
+        TaskVolunteerEntity taskVolunteerEntity = new TaskVolunteerEntity();
+        taskVolunteerEntity.setVolunteerId("1");
+        taskVolunteerEntity.setTaskId("1");
+        taskVolunteerEntity.setChildId("1");
+        taskVolunteerEntity.setApprovalStartTime(new Date(System.currentTimeMillis()));
+        taskVolunteerEntity.setIsCompletedApproval((byte) 1);
+        int res = taskVolunteerEntityMapper.update(taskVolunteerEntity);
+        assert res == 1;
+    }
+
+    @Test
+    public void testSelectAll() {
+        List<TaskVolunteerEntity> list = new ArrayList<TaskVolunteerEntity>();
+        TaskVolunteerEntity taskVolunteerEntity = new TaskVolunteerEntity();
+        taskVolunteerEntity.setVolunteerId("1");
+        taskVolunteerEntity.setChildId("1");
+        list = taskVolunteerEntityMapper.selectAll(taskVolunteerEntity);
+        assert list.size() != 0;
+    }
+
 
 
     }

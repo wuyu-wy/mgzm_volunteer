5421b4b7d72ee9908eb93740074c8c7bb3c4d967
wuyu <viafuture@outlook.com>
2023-11-02 14:30:46 +0800

    最新 更改

--- volunteer/src/main/java/com/blbd/volunteer/dao/LogEntityMapper.java
+++ volunteer/src/main/java/com/blbd/volunteer/dao/LogEntityMapper.java
@@ -1,6 +1,7 @@
 package com.blbd.volunteer.dao;
 
 
+import com.blbd.volunteer.dao.entity.LogEntity;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
@@ -12,7 +13,15 @@ import java.util.List;
 @Repository
 public interface LogEntityMapper {
 
+    //新增日式
+    int insert(LogEntity logEntity);
 
+    //删除日式
+    int delete(LogEntity logEntity);
+
+    //查找所有
+    <List>LogEntity selectAll(LogEntity logEntity);
 
 
 }
+

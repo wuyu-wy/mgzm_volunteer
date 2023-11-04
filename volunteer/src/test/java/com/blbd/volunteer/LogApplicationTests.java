5495e94cdad2a8f34bea737edf459a83aebeb86b
ZincIon <1185911254xj@gmail.com>
2023-11-04 21:32:10 +0800

    新增TaskVolunteerService内容

--- volunteer/src/test/java/com/blbd/volunteer/LogApplicationTests.java
+++ volunteer/src/test/java/com/blbd/volunteer/LogApplicationTests.java
@@ -1,9 +1,7 @@
 package com.blbd.volunteer;
 
 import com.blbd.volunteer.dao.LogEntityMapper;
-import com.blbd.volunteer.dao.VolunteerEntityMapper;
 import com.blbd.volunteer.dao.entity.LogEntity;
-import com.blbd.volunteer.dao.entity.VolunteerEntity;
 import com.blbd.volunteer.utils.UUIDUtil;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
@@ -11,15 +9,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 测试类记得在springbootTest中加上webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
  * SpringBootTest在启动的时候不会启动服务器，所以WebSocket自然会报错，这个时候需要添加选项webEnvironment，以便提供一个测试的web环境。
  */
 @SpringBootTest(classes = VolunteerApplication.class,webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-import java.util.ArrayList;
-import java.util.List;
-
-@SpringBootTest(classes = VolunteerApplication.class)
 @RunWith(SpringRunner.class)
 class LogApplicationTests {
 

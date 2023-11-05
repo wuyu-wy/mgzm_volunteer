5095b4c1428fe78ad14c0775b549a93598ea7d26
ZincIon <1185911254xj@gmail.com>
2023-11-05 15:33:10 +0800

    新增websocket配置与聊天实体类

---
+++ volunteer/src/main/java/com/blbd/volunteer/webSocket/SocketConstants.java
@@ -0,0 +1,24 @@
+package com.blbd.volunteer.webSocket;
+
+public interface SocketConstants {
+    /**
+     * 消息类型
+     */
+    interface MsgType {
+        /**
+         * 消息类型：0心跳 1登录 2文字 3图片 4文件
+         * HEART_BEAT,LOGIN,TEXT_MESSAGE,IMAGE,FILE
+         *
+         */
+        int HEART_BEAT = 0;
+
+        int LOGIN = 1;
+
+        int TEXT_MESSAGE = 2;
+
+        int IMAGE = 3;
+
+        int FILE= 4;
+
+    }
+}
